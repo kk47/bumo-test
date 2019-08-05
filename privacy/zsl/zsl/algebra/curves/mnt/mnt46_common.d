f7bc229a2941706717807cbf9787289560ff9c10
kk47 <kk47yx@gmail.com>
2019-08-05 10:43:49 +0800

    reorganize zsl directory

---
+++ privacy/zsl/zsl/algebra/curves/mnt/mnt46_common.d
@@ -0,0 +1,5 @@
+src/algebra/curves/mnt/mnt46_common.o: \
+ src/algebra/curves/mnt/mnt46_common.cpp \
+ src/algebra/curves/mnt/mnt46_common.hpp src/algebra/fields/bigint.hpp \
+ src/common/serialization.hpp src/common/serialization.tcc \
+ src/common/utils.hpp src/common/utils.tcc src/algebra/fields/bigint.tcc
---
+++ privacy/zsl/zsl/gadgetlib2/infrastructure.d
@@ -0,0 +1,3 @@
+src/gadgetlib2/infrastructure.o: src/gadgetlib2/infrastructure.cpp \
+ src/gadgetlib2/infrastructure.hpp src/common/utils.hpp \
+ src/common/utils.tcc

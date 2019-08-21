5d126017322a2c81f5467626a6281182081b760e
kk47 <kk47yx@gmail.com>
2019-08-21 12:51:46 +0800

    mw first push

---
+++ privacy/mimblewimble/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+#include "secp256k1-zkp/include/secp256k1.h"
+
+int main()
+{
+    std::cout << "secp256k1 test" << std::endl;
+    return 0;
+}
---
+++ privacy/mimblewimble/secp256k1-zkp/CMakeFiles/secp256k1.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/kk/beam/3rdparty/secp256k1-zkp/src/secp256k1.c" "/root/kk/beam/3rdparty/secp256k1-zkp/CMakeFiles/secp256k1.dir/src/secp256k1.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "BC_STATIC"
+  "BOOST_NO_AUTO_PTR"
+  "BOOST_NO_CXX11_SCOPED_ENUMS"
+  "DEBUG_MESSAGES_IN_RELEASE_MODE"
+  "ENABLE_MODULE_ECDH"
+  "ENABLE_MODULE_GENERATOR"
+  "ENABLE_MODULE_RANGEPROOF"
+  "ENABLE_MODULE_RECOVERY"
+  "HAVE_BUILTIN_EXPECT"
+  "PROJECT_SOURCE_DIR=\"/root/kk/beam\""
+  "USE_FIELD_10X26"
+  "USE_FIELD_INV_BUILTIN"
+  "USE_NUM_NONE"
+  "USE_SCALAR_8X32"
+  "USE_SCALAR_INV_BUILTIN"
+  "UV_INTERNAL"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "3rdparty"
+  "3rdparty/libuv/include"
+  "3rdparty/secp256k1-zkp"
+  "3rdparty/secp256k1-zkp/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
